51672839afd1deb9213b914cd196d6935e0f4577
starybog <nikitapishchalin@gmail.com>
2024-11-14 13:58:02 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 n = int(input("школьников: " ))
 k = int(input("яблок: "))
-shkol = n // k
-yabl = n % k 
+shkol = k // n
+yabl = k % n
 print("яблок достанется каждому школьнику", shkol)
 print("яблок останется в корзинке" , yabl)
